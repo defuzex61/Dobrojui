d4efbfd98a46dd1b013003fb3d2da4711e853d50
defuzex61 <isip_r.r.korniyk@mpt.ru>
2023-09-27 12:18:38 +0300

    Добавьте файлы проекта.

---
+++ Dobrojui/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using SerializerLib;
+
+namespace Dobrojui
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ReadButton_Click(object sender, RoutedEventArgs e)
+        {
+            tblock.Text=Serialization.Deserialize<string>();
+            
+        }
+
+        private void SaveButton_Click(object sender, RoutedEventArgs e)
+        {
+            SerializerLib.Serialization.Serialize<string>(tb.Text);
+
+        }
+    }
+}
